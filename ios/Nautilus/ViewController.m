ada859f4fa470b22d14f8075fecfdfc2a0079d15
ǝuıɥsuooɯ <github@ovidiu.me>
2016-07-12 17:29:46 +0800

    The application will now check if the Nautilis plugin is installed on OctoPrint

--- ios/Nautilus/ViewController.m
+++ ios/Nautilus/ViewController.m
@@ -7,35 +7,7 @@
 
 @implementation ViewController
 
-bool settings = NO;
-
-- (void) loadWebView
-{
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSString *url = [defaults stringForKey:@"serverURL"];
-    NSString *apikey = [defaults stringForKey:@"apikey"];
-
-    if ( [apikey length] == 0 || [url length] == 0 ) {
-        settings = NO;
-        
-        NSURL *local_url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"warning" ofType:@"html" inDirectory:@"www"]];
-        //NSLog(@"local_url: %@", local_url);
-        [self.webView loadRequest:[NSURLRequest requestWithURL:local_url]];
-        
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
-    } else {
-        settings = YES;
-
-        if (![url hasSuffix:@"/"] ) {
-            url = [NSString stringWithFormat: @"%@/", url];
-        }
-
-        NSString *goto_url = [NSString stringWithFormat: @"%@?apikey=%@", url, apikey];
-        //NSLog(@"URL %@", goto_url);
-        [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:goto_url]]];
-    }
-
-}
+bool load_webapp = NO;
 
 - (void)viewDidLoad
 {
@@ -48,15 +20,66 @@ bool settings = NO;
     [self loadWebView];
 }
 
+- (void) loadWebView
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *url = [defaults stringForKey:@"serverURL"];
+    NSString *apikey = [defaults stringForKey:@"apikey"];
+    load_webapp = NO;
+    
+    if ( [apikey length] == 0 || [url length] == 0 ) {
+        
+        [self showMessage: @"Please setup the server URL and the API KEY."];
+        
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+        
+    } else {
+
+        if (![url hasSuffix:@"/"] ) {
+            url = [NSString stringWithFormat: @"%@/", url];
+        }
+        
+        NSString *check_url = [NSString stringWithFormat: @"%@plugin/nautilus/static/img/appicon.png", url];
+
+        NSURLSessionConfiguration *nocacheConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        nocacheConfiguration.requestCachePolicy = NSURLRequestReloadIgnoringLocalCacheData;
+        
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:nocacheConfiguration];
+        
+        [[session dataTaskWithURL:[NSURL URLWithString:check_url]
+                completionHandler:^(NSData *data,
+                                    NSURLResponse *response,
+                                    NSError *error) {
+                    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse*)response;
+
+                    if (error) {
+                        [self showMessage: error.localizedDescription];
+                    } else if ([httpResponse statusCode] == 200) {
+                            load_webapp = YES;
+                            NSString *webapp_url = [NSString stringWithFormat: @"%@?apikey=%@", url, apikey];
+                            [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:webapp_url]]];
+                        } else {
+                            [self showMessage: @"This application requires the plugin \"Nautilus\" to be installed on OctoPrint." ];
+                        }
+                }] resume];
+    }
+
+}
+
+-(void)showMessage:(NSString*) message {
+    NSURL *local_url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"message" ofType:@"html" inDirectory:@"www"]] ;
+    NSString *URLString = [local_url absoluteString];
+    NSString *queryString = [NSString stringWithFormat: @"?%@",  [message stringByAddingPercentEncodingWithAllowedCharacters :[NSCharacterSet URLHostAllowedCharacterSet]]];
+    NSString *URLwithQueryString = [URLString stringByAppendingString: queryString];
+    NSURL *finalURL = [NSURL URLWithString: URLwithQueryString];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:finalURL]];
+}
 
 -(void)onForeground:(NSNotification*) notification
 {
-    NSString* javaScript =  @"onForeground()";
-    //NSLog(@"octopi: %@", javaScript);
-    if ( settings ) {
-        //NSLog(@"zooming: %f", self.webView.scrollView.zoomScale);
+    if ( load_webapp ) {
+        NSString* javaScript =  @"onForeground()";
         self.webView.scrollView.zoomScale = 1.0f;
-        //NSLog(@"zooming: %f", self.webView.scrollView.zoomScale);
         [self.webView stringByEvaluatingJavaScriptFromString:javaScript];
     } else {
         [self loadWebView];
@@ -65,9 +88,10 @@ bool settings = NO;
 
 -(void)onBackground:(NSNotification*) notification
 {
-    NSString* javaScript = @"onBackground();";
-    //NSLog(@"octopi: %@", javaScript);
-    [self.webView stringByEvaluatingJavaScriptFromString:javaScript];
+    if ( load_webapp ) {
+        NSString* javaScript = @"onBackground();";
+        [self.webView stringByEvaluatingJavaScriptFromString:javaScript];
+    }
 }
 
 
@@ -79,7 +103,6 @@ bool settings = NO;
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (BOOL)canBecomeFirstResponder {
@@ -89,7 +112,7 @@ bool settings = NO;
 - (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
 {
     if (motion == UIEventSubtypeMotionShake) {
-       [self.webView reload];
+       [self loadWebView];
     }
 }
 
