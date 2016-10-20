44c99b3ba6e88d3a64c80246df9559c422374b78
ǝuıɥsuooɯ <github@ovidiu.me>
2016-10-20 22:23:27 +0800

    fix settigns.ini (2)

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ plugin_requires = []
 
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
 # already be installed automatically if they exist.
-plugin_additional_data = ["settings.ini"]
+plugin_additional_data = ["default"]
 
 
 # Any additional python packages you need to install with your plugin that are not contains in <plugin_package>.*
