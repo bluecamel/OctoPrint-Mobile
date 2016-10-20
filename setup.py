683773b521dce96e390e975ae24cac839604e85a
ǝuıɥsuooɯ <github@ovidiu.me>
2016-10-20 22:07:08 +0800

    include ini file in setup

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ plugin_requires = []
 
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
 # already be installed automatically if they exist.
-plugin_additional_data = []
+plugin_additional_data = ["settings.ini"]
+
 
 # Any additional python packages you need to install with your plugin that are not contains in <plugin_package>.*
 plugin_addtional_packages = []
